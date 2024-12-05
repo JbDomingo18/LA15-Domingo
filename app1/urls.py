c30cd3827aa1b45b50a14364f8f27c6891dc73f3
JbDomingo18 <domingojb18@gmail.com>
2024-12-05 14:02:35 +0800

    Made changes to views and templates

--- app1/urls.py
+++ app1/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('hello/', views.say_hello),     
     path('hi/', views.say_hi), 
-     path('posts/', views.blog_list, name="blog_list"),  # To list posts
+     path('posts/', views.blog_list, name="blog_list"),
     path('posts/<int:id>/', views.blog_detail, name="blog_detail"),
 ]
